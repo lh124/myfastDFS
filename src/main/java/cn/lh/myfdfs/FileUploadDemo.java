9797fd89c23dc89f9b7486a9e132fbcbe7d4a3a1
lh <lh_1215077166@sina.cn>
2017-11-05 00:38:18 +0800

    上传图片的工具类FDS

--- src/main/java/cn/lh/myfdfs/FileUploadDemo.java
+++ src/main/java/cn/lh/myfdfs/FileUploadDemo.java
@@ -1,35 +1,39 @@
 package cn.lh.myfdfs;
 
 import java.io.File;
-import java.util.Arrays;
-
-import org.csource.common.NameValuePair;
-import org.csource.fastdfs.ClientGlobal;
-import org.csource.fastdfs.StorageClient;
-import org.csource.fastdfs.StorageServer;
-import org.csource.fastdfs.TrackerClient;
-import org.csource.fastdfs.TrackerServer;
-import org.springframework.core.io.ClassPathResource;
 
 public class FileUploadDemo {
 	public static void main(String[] args) throws Exception{
-		
-		File file=new File("D:"+File.separator+"221739.jpg");
+		/*	File file=new File("D:"+File.separator+"221739.jpg");
+		byte [] data=new byte[(int)file.length()];
+		InputStream inputStream =new FileInputStream(file);
+		inputStream.read(data);
+		inputStream.close();
 		String ext="jpg";
 		ClassPathResource classPathResource=new ClassPathResource("fdstfds.properties");
 		ClientGlobal.init(classPathResource.getClassLoader().getResource("fdstfds.properties").getPath());
 		TrackerClient trackerClient=new TrackerClient();
 		TrackerServer trackerServer=trackerClient.getConnection();
 		StorageServer storageServer=null;
-		StorageClient client=new StorageClient(trackerServer, storageServer);
-		
+//		StorageClient client=new StorageClient(trackerServer, storageServer);
+		StorageClient1 client=new StorageClient1(trackerServer, storageServer);
+
 		NameValuePair[] nameValuePairs=new NameValuePair[3];
 		nameValuePairs[0]=new NameValuePair("filename",file.getName());
 		nameValuePairs[1]=new NameValuePair("fileExtName",ext);
 		nameValuePairs[2]=new NameValuePair("fileLength",String.valueOf(file.length()));
-		
-		String [] upload_img=client.upload_appender_file(file.getPath(), ext, nameValuePairs);
-		
-		System.out.println(Arrays.toString(upload_img));
+
+//		String [] upload_img=client.upload_file(file.getPath(), ext, nameValuePairs);
+		String upload_img=client.upload_file1(file.getPath(), ext, nameValuePairs);
+		System.out.println(upload_img);*/
+
+
+		upload client = new upload("fdstfds.properties");
+		String result = client.uploadFile("D:"+File.separator+"221739.jpg", "jpg");
+		System.out.println(result);
 	}
+
+
+
+
 }
